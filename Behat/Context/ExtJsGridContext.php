8a8928219b21a2f723597f7813667c829d144beb
Ilya Sustretov <ilya.sustretov@modera.com>
2022-09-05 18:07:27 +0300

    update chrome

--- Behat/Context/ExtJsGridContext.php
+++ Behat/Context/ExtJsGridContext.php
@@ -98,7 +98,7 @@ JS;
 
             $cellDomId = $q->runWhenComponentAvailable("grid[tid=$tid] ", $js);
             $cell = $admin->findElement(By::id($cellDomId));
-            $admin->action()->doubleClick($cell)->perform();
+            $admin->action()->click($cell)->perform();
         });
     }
 
